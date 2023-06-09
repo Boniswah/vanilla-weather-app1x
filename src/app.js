c025eef85e19837d9d641b119320441d7be9fcd3
Boniswah <boniswah@gmail.com>
2023-06-09 18:44:16 +0200

    added js for temp, description,city elements

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
-let city = "Pretoria"
+function displayTemperature(response) {
+console.log(response.data);
+let temperatureElement = document.querySelector("#temperature");
+let cityElement = document.querySelector("#city");
+let descriptionElement = document.querySelector("#description");
+temperatureElement.innerHTML = Math.round (response.data.temperature.current);
+cityElement.innerHTML = (response.data.city);
+descriptionElement.innerHTML = (response.data.condition.description);
+}
+
+
+
+let city = "Pretoria";
 let apikey = "febb10a9ted73c64aff21fo10d593bdc";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
 
-console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
