e3da57f8998bf23c866d6913e2771dc4f175f846
Boniswah <boniswah@gmail.com>
2023-06-09 20:32:29 +0200

    added more js

--- src/app.js
+++ src/app.js
@@ -1,15 +1,26 @@
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let day = date.getDay();
+    return `${day} ${hours}:${minutes}`;
+
+}
+
 function displayTemperature(response) {
-console.log(response.data);
+console.log(response.data.time);
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
 let humidityElement = document.querySelector("#humidity");
 let windElement = document.querySelector("#wind");
+let dateElement = document.querySelector("#date");
 temperatureElement.innerHTML = Math.round (response.data.temperature.current);
 cityElement.innerHTML = (response.data.city);
 descriptionElement.innerHTML = (response.data.condition.description);
 humidityElement.innerHTML = (response.data.temperature.humidity);
 windElement.innerHTML = Math.round (response.data.wind.speed);
+dateElement.innerHTML = formatDate(response.data.time * 1000);
 
 }
 
