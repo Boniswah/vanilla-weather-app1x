219c61d35b8b8adcc8adf9d520348ea93f785e98
Boniswah <boniswah@gmail.com>
2023-06-09 16:31:38 +0200

    added api and url key

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
-alert("What is the temp?");
+let city = "Pretoria"
+let apikey = "febb10a9ted73c64aff21fo10d593bdc";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+
+console.log(apiUrl);
