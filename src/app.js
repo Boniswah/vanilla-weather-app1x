6b5be66aa872cf91388f0e44943834d68c1330bf
Boniswah <boniswah@gmail.com>
2023-06-09 19:36:21 +0200

    added js for wind and humidity

--- src/app.js
+++ src/app.js
@@ -3,9 +3,14 @@ console.log(response.data);
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
 temperatureElement.innerHTML = Math.round (response.data.temperature.current);
 cityElement.innerHTML = (response.data.city);
 descriptionElement.innerHTML = (response.data.condition.description);
+humidityElement.innerHTML = (response.data.temperature.humidity);
+windElement.innerHTML = Math.round (response.data.wind.speed);
+
 }
 
 
