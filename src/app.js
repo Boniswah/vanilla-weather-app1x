effdb2385fabb29b19b128212b6574013f66141d
Boniswah <boniswah@gmail.com>
2023-06-12 10:51:40 +0200

    added forecast HTML to JS

--- src/app.js
+++ src/app.js
@@ -11,8 +11,40 @@ function formatDate(timestamp){
     let days = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saterday"];
     let day = days[date.getDay()];
     return`${day} ${hours}:${minutes}`;
-
 }
+
+function displayForecast() {
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Mon", "Tue", "Wed","Thu"];
+  days.forEach(function(day){
+
+    forecastHTML = forecastHTML +
+      `
+ 
+   <div class="col-2">
+     <div class="weather-forecast-date">${day}</div>
+
+     <img
+       src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/scattered-clouds-day.png"
+       alt=""
+       width="42"
+     />
+<div class="weather-forecast-temperature">
+       <span class="weather-forecast-temperature-max">18°</span>
+       <span class="weather-forecast-temperature-min">12°</span>
+     </div>
+   </div>
+ `;
+});
+
+  
+ forecastHTML = forecastHTML+`</div>`;
+ forecastElement.innerHTML = forecastHTML;
+}
+
+
 
 function displayTemperature(response) {
 
@@ -90,3 +122,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Centurion");
+displayForecast();
